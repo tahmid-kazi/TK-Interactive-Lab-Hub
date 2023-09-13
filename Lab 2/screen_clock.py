f3d587c2a71fc02b2e97b5b9a9314178d5191d90
tahmid-kazi <tk.veritas1@gmail.com>
2023-09-13 07:19:35 -0400

    lab 2 materials

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -2,7 +2,7 @@ import time
 import subprocess
 import digitalio
 import board
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageSequence
 import adafruit_rgb_display.st7789 as st7789
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
@@ -53,26 +53,77 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font_size = 24  # (TK) Adjust the font size as needed.
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", font_size)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+#(TK) added buttons as input
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+#(TK) Images
+image1 = Image.open("images/image1.jpg")
+image1 = image1.resize((width, height))
+image2 = Image.open("images/image2.jpg")
+image2 = image2.resize((width, height))
+pika = Image.open("images/pikachu3.jpg")
+pika = pika.resize((width, height))
+
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    #draw.rectangle((0, 0, width, height), outline=0, fill=5000)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
 
+    # Get the current date.
+    current_date = time.strftime("%m/ %d/ %Y")
+    current_time = time.strftime("%H:%M:%S")
+    
     # Get the current time.
-    current_time = time.strftime('%H:%M:%S')
+    current_weekday = time.strftime("%A")
+    current_hours = time.strftime('%H: hours')
+    current_minutes = time.strftime('%M: mins')
+    current_seconds = time.strftime('%S seconds')
+    
+    #draw.text((40, 10), current_date, fill=(255, 255, 255), font=font)
+    #draw.text((40, 35), current_hours, fill=(255, 255, 255), font=font)
+    #draw.text((40, 60), current_minutes, fill=(255, 255, 255), font=font)
+    #draw.text((40, 85), current_seconds, fill=(255, 255, 255), font=font)
+        
+    if not buttonA.value:
+        display_image = image1.copy()
+        draw_on_image = ImageDraw.Draw(display_image)
+        draw_on_image.text((50, 50), current_weekday, font=font, fill="#FFFFFF")        
+        # Display the modified images with the time on the screen
+        disp.image(display_image, rotation)
+        continue       
 
-    # Draw the time on the image.
-    draw.text((40, 40), current_time, fill=(255, 255, 255), font=font)
+    elif not buttonB.value:
+        display_image = image2.copy()
+        draw_on_image = ImageDraw.Draw(display_image)
+        draw_on_image.text((40, 10), current_date, font=font, fill="#FFFFFF")
+        draw_on_image.text((40, 35), current_hours, font=font, fill="#FFFFFF")
+        draw_on_image.text((40, 60), current_minutes, font=font, fill="#FFFFFF")
+        draw_on_image.text((40, 85), current_seconds, font=font, fill="#FFFFFF")
+    
+        # Display the modified images with the time on the screen
+        disp.image(display_image, rotation)
+        continue
+        
+    else:
+        display_image = pika.copy()
+        draw_on_image = ImageDraw.Draw(display_image)
+        disp.image(display_image, rotation)
+        continue
 
 
     # Display image.
     disp.image(image, rotation)
-    time.sleep(1)
+    time.sleep(0.1)
