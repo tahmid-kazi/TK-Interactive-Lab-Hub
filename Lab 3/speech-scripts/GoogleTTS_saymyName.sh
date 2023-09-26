126f8e69bfaea6ddab2512977d0a246d354d4d03
Tahmid Kazi <101431441+tahmid-kazi@users.noreply.github.com>
2023-09-25 23:38:15 -0400

    Update GoogleTTS_saymyName.sh

--- Lab 3/speech-scripts/GoogleTTS_saymyName.sh
+++ Lab 3/speech-scripts/GoogleTTS_saymyName.sh
@@ -3,5 +3,5 @@
 #!/bin/bash
 say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 #say $*
-say "Hi! My name is Sid."
+say "Hi! My name is TK."
  
